019cf1096e8b58994ca56b14cb1e3dead7213425
Barbary-Theo <theo.barbary25@gmail.com>
2022-10-14 23:20:17 +0200

    Continue to implete game rules. Be carefull there is a bug to fix with the pawn embrassed check

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 from rich.text import Text
-
 from player import Player
+from rich import console
 
 
 class Game:
+    console = console.Console()
 
-    def __init__(self, p1: Player = None, p2: Player = None, nb_row=8, nb_col=8):
+    def __init__(self, p1: Player = None,
+                 p2: Player = None,
+                 nb_row=8,
+                 nb_col=8):
+
         self.p1 = p1
         self.p2 = p2
         self.map = self.init_map(nb_row, nb_col)
@@ -27,15 +32,12 @@ class Game:
 
     def print(self):
 
-        from rich import console
-        console = console.Console()
-
-        console.print("    ", end="")
+        self.console.print("    ", end="")
         for i in range(len(self.map)):
-            console.print("[red]" + str(i) + "[/]", style="underline", end="")
-            console.print("  ", end="")
+            self.console.print("[red]" + str(i) + "[/]", style="underline", end="")
+            self.console.print("  ", end="")
 
-        console.print("")
+        self.console.print("")
         row_number = 0
 
         for row in self.map:
@@ -43,7 +45,7 @@ class Game:
             for col in row:
                 string += str(col) + "  "
             row_number += 1
-            console.print(string)
+            self.console.print(string)
 
     def print_all(self):
         self.print()
@@ -57,18 +59,54 @@ class Game:
         for pawn in self.p2.pawn_set:
             self.map[pawn.get("x")][pawn.get("y")] = "x"
 
-    def start_game(self):
+    def is_pawn_embrassed(self, position: dict, symbol: str):
+        return (
+                (self.map[position.get("x") - 1][position.get("y")] == symbol
+                 and self.map[position.get("x") + 1][position.get("y")] == symbol)
+                or
+                (self.map[position.get("x")][position.get("y") - 1] == symbol
+                 and self.map[position.get("x")][position.get("y") + 1] == symbol)
+                or
+                (self.map[position.get("x") - 1][position.get("y") - 1] == symbol
+                 and self.map[position.get("x") + 1][position.get("y") + 1] == symbol)
+                or
+                (self.map[position.get("x") - 1][position.get("y") + 1] == symbol
+                 and self.map[position.get("x") + 1][position.get("y") - 1] == symbol)
+        )
 
-        from rich import console
-        console = console.Console()
+    def is_a_pawn_have_to_swap_team(self, player_who_played, player_to_check_pawn):
+
+        player_who_played_start_copy = player_who_played
+        player_to_check_pawn_start_copy = player_to_check_pawn
+
+        for position in player_to_check_pawn.pawn_set:
+
+            try:
+                if self.is_pawn_embrassed(position, player_who_played.symbol):
+                    player_to_check_pawn.pawn_set.remove(position)
+                    player_who_played.pawn_set.append(position)
+            except Exception as e:
+                self.console.print(e + " -> ? index out of bounds ? ", style="red")
+
+        if self.p1 == player_to_check_pawn_start_copy:
+            self.p1 = player_who_played
+            self.p2 = player_to_check_pawn
+        else:
+            self.p1 = player_who_played
+            self.p2 = player_to_check_pawn
+
+        return True
+
+    def start_game(self):
 
         is_game_terminate = False
 
         while not is_game_terminate:
-            console.print(
+            self.console.print(
                 Text(
-                    "\n-------------------------------------------------------------------------------- " + self.p1.name
-                    + " have to play ---------------------------------------------------------------------\n"),
+                    "\n-------------------------------------------------------------------------------- "
+                    + self.p1.name + " have to play (" + self.p1.symbol +
+                    ") ---------------------------------------------------------------------\n"),
                 justify="center", style="green"
             )
 
@@ -79,11 +117,15 @@ class Game:
             else:
                 self.p1.IA_play(self.map)
             self.update_map()
+            self.is_a_pawn_have_to_swap_team(self.p1, self.p2)
+            self.is_a_pawn_have_to_swap_team(self.p2, self.p1)
+            self.update_map()
 
-            console.print(
+            self.console.print(
                 Text(
-                    "\n-------------------------------------------------------------------------------- " + self.p2.name
-                    + " have to play ---------------------------------------------------------------------\n"),
+                    "\n-------------------------------------------------------------------------------- "
+                    + self.p2.name + " have to play (" + self.p1.symbol +
+                    ") ---------------------------------------------------------------------\n"),
                 justify="center", style="cyan"
             )
 
@@ -94,3 +136,7 @@ class Game:
             else:
                 self.p2.IA_play(self.map)
             self.update_map()
+            self.is_a_pawn_have_to_swap_team(self.p1, self.p2)
+            self.is_a_pawn_have_to_swap_team(self.p2, self.p1)
+            self.update_map()
+
--- player.py
+++ player.py
@@ -1,67 +1,62 @@
-from rich.markdown import Markdown
-
+from rich import console
 
 class Player:
 
-    def __init__(self, name: str = None, couleur: str = "white", pawn_set: list = None, type: str = "real"):
+    console = console.Console()
+
+    def __init__(self, name: str = None,
+                 couleur: str = "white",
+                 pawn_set: list = None,
+                 type: str = "real",
+                 symbol: str = "o"):
+
         if pawn_set is None:
             pawn_set = []
         self.name = name
         self.couleur = couleur
         self.pawn_set = pawn_set
         self.type = type
+        self.symbol = symbol
 
     def print(self):
-
-        from rich import console
-        console = console.Console()
-
         set = ""
         for object in self.pawn_set:
             set += "\n    {x: \"" + str(object.get("x")) + "\", y: \"" + str(object.get("y")) + "\"}"
 
-        console.print(
+        self.console.print(
             "player { \n  name: \"" + self.name + "\",\n  couleur: \"" + self.couleur + "\"\n  pawn: [" + set + "\n  ]\n}")
 
     def play(self, map):
-        from rich import console
-        console = console.Console()
 
         is_a_good_placement = False
 
         while not is_a_good_placement:
 
-            console.print("\nChoose a position to put your pawn", style="yellow")
+            self.console.print("\nChoose a position to put your pawn", style="yellow")
             try:
-                console.print("-> Column number : ", style="yellow", end="")
+                self.console.print("-> Column number : ", style="yellow", end="")
                 y = int(input())
-                console.print("-> Row number : ", style="yellow", end="")
+                self.console.print("-> Row number : ", style="yellow", end="")
                 x = int(input())
                 nice_input = True
 
             except Exception as e:
                 nice_input = False
+                x= 3
                 y = 3
-                x = 3
 
             if nice_input:
                 try:
-                    row = map[x]
-                    cell = row[y]
-                    if cell == ".":
+                    if map[x][y] == ".":
                         is_a_good_placement = True
                         self.pawn_set.append({"x": x, "y": y})
                     else:
-                        console.print("❌ Position already token ❌", style="red")
-                        console.print()
+                        self.console.print("❌ Position already token ❌", style="red")
+                        self.console.print()
                 except Exception as e:
-                    console.print("Please check your inputs\n", style="red")
+                    self.console.print("Please check your inputs\n", style="red")
             else:
-                console.print("Please check your inputs\n", style="red")
+                self.console.print("Please check your inputs\n", style="red")
 
     def IA_play(self, map):
-
-        from rich import console
-        console = console.Console()
-
-        console.print("\n⚠️Functionality to develop ⚠️\n", style="red")
+        self.console.print("\n⚠️Functionality to develop ⚠️\n", style="red")
